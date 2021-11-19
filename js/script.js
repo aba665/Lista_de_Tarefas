59255f3943fc2836fc813da62e39f8b26f663b03
Abisson Santos <abissons@gmail.com>
2021-11-19 19:48:43 -0300

    all

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function Diminuir(){
+    var small = document.getElementById("o")
+    var small2 = document.getElementById("u")
+    small.style.height = `30vw` 
+    small2.style.height = `30vw`
+    // small.addEventListener("mouseout", function normal(){
+    //     small.style.height = `40vw`
+    //     small2.style.height = `40vw`
+    // })
+}
+function Aumentar(){
+    var big = document.getElementById("meio")
+    big.style.height = `40vw`
+}
