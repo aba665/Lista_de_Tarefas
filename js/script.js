8e2fdeb375965740f97669159efd7a0f82643059
Abisson Santos <abissons@gmail.com>
2021-11-20 09:13:21 -0300

    New changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,62 @@
+var small = document.querySelectorAll("div.encima")
+var big = document.getElementById("meio")
+var to_do = document.getElementById("fazer")
+var to_doing = document.getElementById("fazendo")
+var to_finished = document.getElementById("feito") 
+
 function Diminuir(){
-    var small = document.getElementById("o")
-    var small2 = document.getElementById("u")
-    small.style.height = `30vw` 
-    small2.style.height = `30vw`
-    // small.addEventListener("mouseout", function normal(){
-    //     small.style.height = `40vw`
-    //     small2.style.height = `40vw`
-    // })
+
+    for(let i = 0; i < 2; i++){
+        small[i].style.height = `30vw` 
+    }
+
 }
+function leave(){
+
+    for(let i = 0; i < 2; i++){
+        small[i].style.height = `40vw` 
+    }
+
+ }
+
 function Aumentar(){
-    var big = document.getElementById("meio")
+
     big.style.height = `40vw`
+    console.log("oi")
+
+}
+function MediumSmall(){
+
+    big.style.height = `32vw`
+    console.log("oi")
+
+
+}
+function InserirDados(){
+    let txt = document.createElement("p")
+    txt.innerHTML = " "
+    first.appendChild(txt)
+    txt.innerHTML += `${to_do.value}`
+    txt.style.background = `white`
+    to_do.value= ""
+    to_do.focus()
+}
+function InserirDados2(){
+    let txt2 = document.createElement("p")
+    txt2.innerHTML = " "
+    second.appendChild(txt2)
+    txt2.innerHTML += `${to_doing.value}`
+    txt2.style.background = `white`
+    to_doing.value= ""
+    to_doing.focus()
+}
+function InserirDados3(){
+    let txt3 = document.createElement("p")
+    txt3.innerHTML = " "
+    third.appendChild(txt3)
+    txt3.innerHTML += `${to_finished.value}`
+    txt3.style.background = `white`
+    to_finished.value= ""
+    to_finished.focus()
+
 }
