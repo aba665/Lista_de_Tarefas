cb634124f97ab11a4a3e4d72066ec97784e23ce3
Abisson Santos <abissons@gmail.com>
2021-11-21 16:10:51 -0300

    all

--- js/script.js
+++ js/script.js
@@ -2,47 +2,40 @@ var small = document.querySelectorAll("div.encima")
 var big = document.getElementById("meio")
 var to_do = document.getElementById("fazer")
 var to_doing = document.getElementById("fazendo")
-var to_finished = document.getElementById("feito") 
-
-function Diminuir(){
-
-    for(let i = 0; i < 2; i++){
-        small[i].style.height = `30vw` 
-    }
-
-}
-function leave(){
-
-    for(let i = 0; i < 2; i++){
-        small[i].style.height = `40vw` 
-    }
-
- }
+var to_finished = document.getElementById("feito")
+var arr = []
+var arr2 = []
 
 function Aumentar(){
 
     big.style.height = `40vw`
-    console.log("oi")
-
-}
-function MediumSmall(){
-
-    big.style.height = `32vw`
-    console.log("oi")
-
+    for(let i = 0; i < 2; i++){
+                small[i].style.height = `40vw` 
+            }
 
 }
 function InserirDados(){
-    let txt = document.createElement("p")
-    txt.innerHTML = " "
+    arr.push(to_do.value)
+    var txt = document.createElement("li")
+    let imagem = document.createElement("img")
+    imagem.setAttribute("id", "imginside")
+    imagem.setAttribute("src", "/image/delete.png")
     first.appendChild(txt)
     txt.innerHTML += `${to_do.value}`
     txt.style.background = `#6088c4fb`
     to_do.value= ""
     to_do.focus()
+    txt.appendChild(imagem)
+
+    
 }
 function InserirDados2(){
-    let txt2 = document.createElement("p")
+    var txt2 = document.createElement("li")
+    let imagem = document.createElement("img")
+    txt2.setAttribute("class", "oi")
+    imagem.setAttribute("id", "imginside")
+    imagem.setAttribute("src", "/image/delete.png")
+    imagem.setAttribute("onclick", "trash()")
     txt2.innerHTML = " "
     second.appendChild(txt2)
     txt2.innerHTML += `${to_doing.value}`
@@ -50,14 +43,40 @@ function InserirDados2(){
     to_doing.value= ""
     txt2.style.textShadow = `2px 1px 2px black`
     to_doing.focus()
+    txt2.appendChild(imagem)
 }
 function InserirDados3(){
-    let txt3 = document.createElement("p")
+    var txt3 = document.createElement("li")
+    let imagem = document.createElement("img")
+    imagem.setAttribute("id", "imginside")
+    imagem.setAttribute("src", "/image/delete.png")
     txt3.innerHTML = " "
     third.appendChild(txt3)
     txt3.innerHTML += `${to_finished.value}`
     txt3.style.background = `#6088c4fb`
     to_finished.value= ""
     to_finished.focus()
+    txt3.appendChild(imagem)
+}
+
+function DeleteBlocoFirst(){
+
+    let blocoFirst = document.getElementById('first')
+    blocoFirst.innerHTML = ""
 
 }
+
+function DeleteBlocoSecond(){
+
+     let blocoSecond = document.getElementById('second')
+     blocoSecond.innerHTML = ""
+
+}
+
+function DeleteBlocoThird(){
+
+    let blocoThird = document.getElementById('third')
+    blocoThird.innerHTML = ""
+
+}
+console.log(arr)
